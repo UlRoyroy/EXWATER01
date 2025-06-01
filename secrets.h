82103f7782b88726192d1256977c2ada8ff79f5b
UlRoyroy <119036813+UlRoyroy@users.noreply.github.com>
2025-06-02 01:48:10 +0900

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,18 @@
+// secrets.h
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi
+#define WIFI_SSID "YourWiFiName"
+#define WIFI_PASSWORD "YourPassword"
+
+// API Key
+#define API_KEY "AIzaSyDp34Lp0aJQN-k8eOkyKcvRuxrLJPSaHDE"
+
+// MQTT 또는 기타 비밀번호
+#define MQTT_USERNAME "yourMqttUser"
+#define MQTT_PASSWORD "yourMqttPass"
+
+#endif
+
